60912a6842211b0ca0a3e8504c967dcf3a37e1b0
Sita Tan <sita_tan@epam.com>
2022-10-21 16:42:40 +0800

    docs(most of files): initialize the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default App
